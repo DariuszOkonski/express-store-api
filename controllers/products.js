d788b11ac39ec56edc03bfbeed61c2537ba98d26
Dariusz Okoński <darek200180@gmail.com>
2024-05-09 19:42:47 +0200

    add filter by name

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,31 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ name: 'high-back bench' });
-  return res.status(200).json({ products, nbHits: products.length });
+  const search = 'ab';
+
+  const products = await Product.find({
+    name: { $regex: search, $options: 'i' },
+  });
+  return res.status(200).json({ nbHits: products.length, products });
 };
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query);
+  const { featured, company, name } = req.query;
+  const queryObject = {};
 
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
+
+  const products = await Product.find(queryObject);
   return res.status(200).json({ nbHits: products.length, products });
 };
 
