4611d063db355ab9663e5d6f0a42491f26ac6d39
Dariusz Okoński <darek200180@gmail.com>
2024-05-11 11:44:13 +0200

    add page limit

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).select('name price');
+  const products = await Product.find({}).sort('name').select('name price');
   return res.status(200).json({ nbHits: products.length, products });
 };
 
@@ -34,6 +34,12 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(',').join(' ');
     result = result.select(fieldsList);
   }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
 
   const products = await result;
   return res.status(200).json({ nbHits: products.length, products });
